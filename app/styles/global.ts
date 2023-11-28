5dc7cb4a8b86e780e6f9ef2ce86d9ba14a4a3f47
Thiago Camargo <thiago.camargo@outlook.com>
2023-11-28 17:11:05 -0300

    firts commit

---
+++ app/styles/global.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        text-decoration: none;
+        box-sizing: border-box;
+    }
+`;
+
+export default GlobalStyles;
